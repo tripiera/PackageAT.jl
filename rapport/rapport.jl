248ce616c9f7b9caf0ab7fdeeebb93bf7f7132ca
tripiera <anaide.tripier@sfr.fr>
2025-12-07 17:47:14 +0100

    d

--- rapport/rapport.jl
+++ rapport/rapport.jl
@@ -1,2 +1,6 @@
 using BonitoBook  
 BonitoBook.book("projet_rapport.md")
+
+#voir projet_rapport.md dans packageat.jl/ .je n'arrive pas à afficher projet_rapport.md
+# tout en le mettant dans le meme dossier que rapport.jl . donc il faut juste 
+#lancer ce code et ca marche tant pis pour la presentation
