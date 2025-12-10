4b8f03c10d37e1cce2e97d6f0c350c1a43da81cd
tripiera <anaide.tripier@sfr.fr>
2025-12-10 21:36:50 +0100

    aef

--- src/PackageAT.jl
+++ src/PackageAT.jl
@@ -5,7 +5,10 @@ using CSV
 export ask_mbti_questions
 include("types_projet.jl")
 include("compatibilité.jl")
-
+include("1bonito_compa.jl")
+include("final_bonito.jl")
+include("trouver_match.jl")
+include("calcul_compatibilite.jl")
 
 # Write your package code here.
 
